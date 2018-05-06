76314d32ac9cfaf151ed1aa4a339e37bf4b00bc8
Ethan Gaebel <egaebel@vt.edu>
2018-05-06 14:07:29 -0700

    Fixed pdfs being dumped to home directory and added code to set file names from subject line

--- mail-sync.sh
+++ mail-sync.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BASE_DIR="/home/egaebel/.mail/paper-to-push/new/"
+BASE_DIR="/home/egaebel/.mail/paper-to-push/new"
 PAPERS_DIR="/home/egaebel/workspace/Papers/paper-push-bot-drop-spot"
 
 cur_seconds=$(date +%s)
 echo "Current seconds: $cur_seconds"
 
 # Sync mail from imap and save to dir
-offlineimap && munpack /home/egaebel/.mail/paper-to-push/new/*
+offlineimap
 
 ls_file_names=$(ls $BASE_DIR)
 readarray -t file_names < <( echo "$ls_file_names" )
@@ -26,7 +26,16 @@ for i in $(seq 0 "${#file_names[@]}"); do
     filtered_file_name="${file_names[$i]}"
     timestamp="${timestamps[$i]}"
     if [[ "$timestamp" -gt "$cur_seconds" ]]; then
-        echo "Extracting from file name: $filtered_file_name"
-        mu extract -a --target-dir="$PAPERS_DIR" "$BASE_DIR$filtered_file_name"
+        echo "Extracting from file name: |$filtered_file_name|"
+        attachment_file_name=$(mu extract "$BASE_DIR"/"$filtered_file_name"\
+            | grep "\[attach\]"\
+            | cut -d " " -f4)
+        echo "Extracting attachment with file name: |$attachment_file_name|....."
+        mu extract -a --target-dir="$PAPERS_DIR" "$BASE_DIR/$filtered_file_name"
+        
+        # Extract subject line and use as the file name after triming whitespace
+        subject_file_name=$(grep "Subject: " "$BASE_DIR"/"$filtered_file_name" | cut -d ":" -f2 | sed 's/^[[:space:]]*//' | sed 's/$[[:space:]]*//')
+        echo "Renaming attachment with subject: |$subject_file_name|....."
+        mv "$PAPERS_DIR"/"$attachment_file_name" "$PAPERS_DIR"/"$subject_file_name"
     fi
 done
